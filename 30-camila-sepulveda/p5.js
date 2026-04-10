03d3696cde7012da92d0e45cb18014a54051d0b7
camilasepulvedab <camila.sepulveda5@mail.udp.cl>
2026-04-10 10:58:32 -0400

    Update p5.js

--- 30-camila-sepulveda/p5.js
+++ 30-camila-sepulveda/p5.js
@@ -2,3 +2,5 @@
 
 ## posición del mouse
 (mousrX,mouseY);      ej: ellipse(mouseX,mouseY,100,100);
+
+para que hyaya una estela de la figura cuando se mueve con el mouse, el background debe estar arriba en setup, pero para que no deje estela y solo le mueva la figura se deja en draw
